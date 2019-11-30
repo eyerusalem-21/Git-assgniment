5dbb768d702792f8fee0eb3e7af8975ff0ac8494
emran-11 <58083561+emran-11@users.noreply.github.com>
2019-11-30 09:04:37 +0300

    Update project1.java

--- project1.java
+++ project1.java
@@ -9,11 +9,6 @@ public class CompareTwoNumbers {
 		if(num1 > num2){
 			System.out.println(num1 + " is greater than " + num2);
 		}
-		else if(num1 < num2){
-			System.out.println(num1 + " is less than " + num2);
-		}
-		else{
-			System.out.println(num1 + " is equal to " + num2);
-		}
+		
 	}
 }
